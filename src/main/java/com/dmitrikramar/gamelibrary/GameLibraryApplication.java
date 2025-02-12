e77c2ece7a406a7e30eb1a1ff33d12194abf92d9
Dmitri Kramar <dmitri.kramar@outlook.com>
2025-02-12 19:28:38 +0200

    Initial project setup

---
+++ src/main/java/com/dmitrikramar/gamelibrary/GameLibraryApplication.java
@@ -0,0 +1,15 @@
+package com.dmitrikramar.gamelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*Main class for starting the Spring Boot application.*/
+
+@SpringBootApplication
+public class GameLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameLibraryApplication.class, args);
+	}
+
+}
